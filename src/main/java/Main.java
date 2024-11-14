112c7abc735b07b6cb5f296c3a9df1692b707f16
Luisa Leon Campos <luisaleon34563@gmail.com>
2024-11-14 21:30:58 +0000

    subiendo proyecto

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    double precioTotal = 660 ;
+    double porcentaje = precioTotal * 0.1;
+    double precioConDescuento = precioTotal -              porcentaje;
+
+    System.out.println("El valor a pagar es: " +      precioConDescuento + "€");
+  }
+
+ 
+}
